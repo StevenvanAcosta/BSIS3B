017fddddc488a65d6932d253eb5e9d248f1f7198
gen <causingrachel@gmail.com>
2024-03-22 08:55:37 +0800

    updated

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -4,5 +4,10 @@
 require 'functions.php';
 require 'App.php';
 require 'Controller.php';
+require 'Database.php';
+require 'Model.php';
 
+spl_autoload_register(function ($class_name) {
 
+    require'../app/models/' . $class_name. 'php';
+});
