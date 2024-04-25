2bd855bdee4e067c4170d921689109ea728f59cf
StevenvanAcosta <stevenacosta0203@gmail.com>
2024-04-25 08:16:09 -0700

    updated list-of-tasks

--- list-of-tasks/app/core/config.php
+++ list-of-tasks/app/core/config.php
@@ -1,7 +1,7 @@
 <?php
     define('APP_NAME', 'Task Manager');
     define('APP_LOGO', 'App-Logo.png');
-    define('ROOT', 'http://'. $_SERVER['SERVER_NAME'] .'/Web/WebA-MVC/list-of-tasks/public');
+    define('ROOT', 'http://'. $_SERVER['SERVER_NAME'] .'/GitRepo/list-of-tasks/public');
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'task_db');
     define('DB_USER', 'root');
