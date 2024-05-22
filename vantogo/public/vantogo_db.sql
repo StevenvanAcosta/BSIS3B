a69e0d5b93cf953324450560f627d3f5b1e64876
StevenvanAcosta <stevenacosta0203@gmail.com>
2024-05-22 08:30:38 -0700

    database

---
+++ vantogo/public/vantogo_db.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2024 at 05:27 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vantogo_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id` int(100) NOT NULL,
+  `location` text NOT NULL,
+  `contact` text NOT NULL,
+  `pick-up-date` varchar(100) NOT NULL,
+  `return-date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `top-deals`
+--
+
+CREATE TABLE `top-deals` (
+  `deal_id` int(11) NOT NULL,
+  `pick_up_date` date NOT NULL,
+  `return_date` date NOT NULL,
+  `pick_up_location` text NOT NULL,
+  `return_location` text NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phonenumber` varchar(11) NOT NULL,
+  `destination` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vans`
+--
+
+CREATE TABLE `vans` (
+  `van_id` int(11) NOT NULL,
+  `van_name` varchar(50) NOT NULL,
+  `van_price` int(11) NOT NULL,
+  `varn_color` varchar(50) NOT NULL,
+  `van_transmission` varchar(50) NOT NULL,
+  `van_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `top-deals`
+--
+ALTER TABLE `top-deals`
+  ADD PRIMARY KEY (`deal_id`);
+
+--
+-- Indexes for table `vans`
+--
+ALTER TABLE `vans`
+  ADD PRIMARY KEY (`van_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `top-deals`
+--
+ALTER TABLE `top-deals`
+  MODIFY `deal_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vans`
+--
+ALTER TABLE `vans`
+  MODIFY `van_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
